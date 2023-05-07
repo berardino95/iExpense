c7803a3258432c98a0f3f239dd25b26d8c1469d8
Berardino Chiarello <berardino95@gmail.com>
2023-05-07 21:20:27 +0200

    Starting project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by CHIARELLO Berardino - ADECCO on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
